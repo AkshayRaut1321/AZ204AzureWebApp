d4ea106b48cb3c49408bf6f4b2f2fa7373832e49
Akshay Raut <rakshay08@gmail.com>
2024-03-10 22:58:37 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using TestAutoScaleOutApp.Models;
+
+namespace TestAutoScaleOutApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly IHostEnvironment _hostEnvironment;
+        private readonly IFeatureManager _featureManager;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration, IHostEnvironment hostEnvironment,
+            IFeatureManager featureManager)
+        {
+            _configuration = configuration;
+            _logger = logger;
+            _hostEnvironment = hostEnvironment;
+            _featureManager = featureManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string? connString;
+            if (_hostEnvironment.IsDevelopment())
+            {
+                //For retrieving connection string from in-app appsettings.json file
+                connString = _configuration.GetConnectionString("SqlConMain");
+                //connString = _configuration.GetConnectionString("SqlConStaging");
+            }
+            else
+            {
+                //If you want to retrieve connection string from Environment variables section of a web app.
+                //connString = _configuration.GetConnectionString("SqlCon");
+
+                //If you want to retrieve from Azure App Configuration resource.
+                connString = _configuration["SqlCon"];
+            }
+            string query = "SELECT Id, Name, Price FROM dbo.Product";
+            DataTable dataTable = new DataTable();
+            var products = new List<Product>();
+
+            try
+            {
+                // Create an instance of SqlDataAdapter to execute the query and fill the DataTable
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connString))
+                {
+                    // Open the connection, execute the query and fill the DataTable,
+                    // then close the connection automatically when done.
+                    adapter.Fill(dataTable);
+                }
+
+                // Now the DataTable is filled with data from the Products table.
+                // You can access the data like this:
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    var product = new Product();
+                    product.Id = Convert.ToInt32(row["Id"]);
+                    product.Name = row["Name"].ToString();
+                    product.Price = Convert.ToInt32(row["Price"]);
+                    products.Add(product);
+                }
+                ViewData["IsBeta"] = await _featureManager.IsEnabledAsync("IsBeta");
+            }
+            catch (Exception ex)
+            {
+                // Handle any errors that might have occurred
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        //If this feature flag is not present or false then server will return 404 if this API method is called.
+        [FeatureGate("IsBeta")]
+        public IActionResult ExecuteRazorFunction()
+        {
+            var parallelOps = new ParallelOptions();
+            parallelOps.MaxDegreeOfParallelism = 10;
+            Parallel.For(0, 10000, (a) =>
+            {
+                Console.Write("test");
+            });
+            // Your function logic here
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration.FeatureManagement;
+using Microsoft.FeatureManagement;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var azureAppConfigConnectionString = "Endpoint=https://testappconfigakshay.azconfig.io;Id=jQjs;Secret=wF4/dGhYNb3yNOPEguNd6Bl/AZze8vdOU72fvtfKKH4=";
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+#region Example of Configurations stored in Azure App Configuration
+//Install nuget named Microsoft.Extensions.Configuration.AzureAppConfiguration
+// 1. Connect to AzureAppConfig using this
+//      builder.Configuration.AddAzureAppConfiguration(azureAppConfigConnectionString);
+// 2. OR using this
+//      builder.Host.ConfigureAppConfiguration(builder =>
+//      {
+//          builder.AddAzureAppConfiguration(azureAppConfigConnectionString);
+//      });
+#endregion
+
+#region Example of Features flags in Azure App Configuration
+//Install Microsoft.Extensions.Configuration.AzureAppConfiguration
+// and Microsoft.FeatureManagement
+builder.Host.ConfigureAppConfiguration(builder =>
+{
+    builder.AddAzureAppConfiguration((options) =>
+    {
+        options.Connect(azureAppConfigConnectionString)
+            .UseFeatureFlags();
+    });
+});
+builder.Services.AddFeatureManagement();
+#endregion
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
